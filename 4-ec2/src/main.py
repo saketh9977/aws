4701abfc35c338b886bc82fcf9df0fc369f0c79e
Saketh <saketh9977@gmail.com>
2023-04-07 13:09:30 +0530

    ec2: added ssm agent status check, tested executing commands on ec2

--- 4-ec2/src/main.py
+++ 4-ec2/src/main.py
@@ -4,6 +4,8 @@ import time
 
 import boto3
 
+aws_region = 'us-east-1'
+
 def print___(msg):
     timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z')
     print(f"{timestamp}: {msg}")
@@ -51,16 +53,125 @@ def poll_ec2_status(ec2_client, instance_id, desired_state_list, undesired_state
         # pprint.pprint(response)
 
         if response['Reservations'][0]['Instances'][0]['InstanceId'] != instance_id:
-            raise(f'instance id = {instance_id} is missing in response')
+            raise Exception(f'instance id = {instance_id} is missing in response')
         
         status = response['Reservations'][0]['Instances'][0]['State']['Name']
         print___(f'instance id = {instance_id}, status = {status}')
 
         if status in undesired_state_list:
-            raise(f'instance id = {instance_id}, status = {status}')
+            raise Exception(f'instance id = {instance_id}, status = {status}')
 
         if status in desired_state_list:
+            print___(f'not polling as instance status = {status}')
             return status
+
+        print___(f'sleeping for {poll_freq_in_sec}s...')
+        time.sleep(poll_freq_in_sec)
+        print___('awake')
+
+def wait_for_ssm_agent(ssm_client, instance_id, desired_state_list, undesired_state_list, poll_freq_in_sec):
+    
+    """
+        If PingStatus = Online then SSM agent is online on that EC2
+        SSM agent listens for incoming commands and executes them.
+    """
+
+    while True:
+
+        print___(f"fetching SSM agent status on instance id = {instance_id}...")
+
+        response = ssm_client.describe_instance_information(
+            InstanceInformationFilterList=[
+                {
+                    'key': 'InstanceIds',
+                    'valueSet': [instance_id]
+                }
+            ]
+        )
+
+        if len(response['InstanceInformationList']) <= 0:
+            continue
+
+        if response['InstanceInformationList'][0]['InstanceId'] != instance_id:
+            continue
+
+        ssm_agent_status = response['InstanceInformationList'][0]['PingStatus']
+        print___(f"ssm agent = {ssm_agent_status}")
+
+        if ssm_agent_status in desired_state_list:
+            print___(f"Not polling as SSM Agent is {ssm_agent_status}")
+            return
+        
+        if ssm_agent_status in undesired_state_list:
+            raise Exception(f"SSM agent {ssm_agent_status}")
+        
+def execute_commands_on_ec2(ssm_client, command_list, instance_id):
+
+    """
+        references
+            1. CLI examples - https://docs.aws.amazon.com/cli/latest/reference/ssm/send-command.html
+            2. Boto3 send_command API syntax - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm/client/send_command.html
+            3. execution timeout - https://github.com/boto/boto3/issues/1343#issuecomment-345778729
+    """
+
+    print___(f'running the following commands on ec2 instance id = {instance_id} -')
+    pprint.pprint(command_list)
+
+    response = ssm_client.send_command(
+        InstanceIds=[instance_id],
+        DocumentName='AWS-RunShellScript',
+        TimeoutSeconds=180,
+        Parameters={
+            'commands': command_list,
+            'executionTimeout': ['5400']
+        },
+        OutputS3BucketName='test-4323',
+        OutputS3KeyPrefix='ssm-logs'
+    )
+    command_id = response['Command']['CommandId']
+    print___(f'triggered, command id = {command_id}')
+
+    return command_id
+
+def poll_ec2_commands(ssm_client, command_id, instance_id, poll_freq_in_sec):
+
+    undesired_state_list = ['Cancelled', 'TimedOut', 'Failed']
+    desired_state_list = ['Success']
+
+    while True:
+        print___(f'fetching command status command id = {command_id}...')
+
+        response = ssm_client.get_command_invocation(
+            CommandId=command_id,
+            InstanceId=instance_id
+        )
+
+        status = response['Status']
+        print___(f"command id = {command_id} execution status = {status}")
+
+        if status in undesired_state_list:
+            print___(f"STDOUT content = ")
+            print___(response['StandardOutputContent'])
+
+            print___(f"STDERR content = ")
+            print___(response['StandardErrorContent'])
+
+            print___(f"STDOUT = {response['StandardOutputUrl']}")
+            print___(f"STDERR = {response['StandardErrorUrl']}")
+            raise Exception(f"command id = {command_id} whose exec. status = {status} on ec2 instance id = {instance_id}")
+        
+        if status in desired_state_list:
+            print___(f'not polling as command exec. status = {status}')
+
+            print___(f"STDOUT content = ")
+            print___(response['StandardOutputContent'])
+
+            print___(f"STDERR content = ")
+            print___(response['StandardErrorContent'])
+
+            print___(f"STDOUT = {response['StandardOutputUrl']}")
+            print___(f"STDERR = {response['StandardErrorUrl']}")
+            return
 
         print___(f'sleeping for {poll_freq_in_sec}s...')
         time.sleep(poll_freq_in_sec)
@@ -78,22 +189,31 @@ def terminate_ec2(ec2_client, instance_id):
 
 def main():
     print___('main: starting...')
+    ec2_client = boto3.client('ec2', region_name=aws_region) 
 
-    ec2_client = boto3.client('ec2', region_name='us-east-1') 
     instance_id = launch_ec2(ec2_client)
-
     desired_state_list = ['running']
     undesired_state_list = ['terminated', 'stopped']
-    poll_ec2_status(ec2_client, instance_id, desired_state_list, undesired_state_list, 10)
-
+    poll_ec2_status(ec2_client, instance_id, desired_state_list, undesired_state_list, 20)
     
+    ssm_client = boto3.client('ssm', aws_region)
+    desired_state_list = ['Online']
+    undesired_state_list = ['ConnectionLost', 'Inactive']
+    wait_for_ssm_agent(ssm_client, instance_id, desired_state_list, undesired_state_list, 5)
+
+    command_list = [
+        'date',
+        'pwd',
+        'aws s3 ls s3://test-4323/',
+        'date'
+    ]
+    command_id = execute_commands_on_ec2(ssm_client, command_list, instance_id)
+    poll_ec2_commands(ssm_client, command_id, instance_id, 10)
 
     terminate_ec2(ec2_client, instance_id)
-
     desired_state_list = ['terminated']
-    undesired_state_list = ['running', 'stopped']
-    poll_ec2_status(ec2_client, instance_id, desired_state_list, undesired_state_list, 10)
-
+    undesired_state_list = ['stopped']
+    poll_ec2_status(ec2_client, instance_id, desired_state_list, undesired_state_list, 20)
 
     print___('main: ending...')
 
